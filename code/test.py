6a136e05399b7edbae0a811b3a395a01de6d2fb1
Beibei Chen <beichen@homeaway.com>
2018-11-24 22:09:04 -0600

    finish 3 exercises

--- code/test.py
+++ code/test.py
@@ -4,13 +4,22 @@
 import unittest
 import array_and_string as aas
 
+
 class TestArrayAndString(unittest.TestCase):
+
     def test_is_unique(self):
         self.assertTrue(aas.is_unique('abdhujsik'))
         self.assertFalse(aas.is_unique('aaaaa'))
         self.assertFalse(aas.is_unique(''))
 
+    def test_check_permutation(self):
+        self.assertTrue(aas.check_permutation('aabbccdd', 'abcdabcd'))
+        self.assertTrue(aas.check_permutation('a', 'a'))
+        self.assertTrue(aas.check_permutation('', ''))
+        self.assertFalse(aas.check_permutation('abcdef', 'abcdff'))
 
+    def test_urlify(self):
+        self.assertEqual(aas.urlify("This is an apple       ", 16), "This%20is%20an%20apple")
 
 
 if __name__ == '__main__':
